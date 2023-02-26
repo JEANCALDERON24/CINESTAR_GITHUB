497c6560c12ae2841e995880e0873e56dd7097f8
USER <USER@DESKTOP-TJSJ0HQ>
2023-02-26 15:25:28 -0500

    Actualizacion

--- src/main/java/svl/svlPelicula.java
+++ src/main/java/svl/svlPelicula.java
@@ -8,16 +8,15 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-//-svlPelicula-//
-
 public class svlPelicula extends HttpServlet {
-private static final long serialVersionUID = 1L;
-         
-public svlPelicula() {
-super();
-}
-
-protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public svlPelicula() {
+        super();
+    
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	response.setContentType("text-html;charset=UTF-8");
     	response.setCharacterEncoding("UTF-8");
     	HttpSession session = request.getSession();
@@ -25,21 +24,22 @@ protected void processRequest(HttpServletRequest request, HttpServletResponse re
     	String id =request.getParameter("id");
     	if(id !=null && (id.equals("cartelera") || id.equals("estrenos"))) {
      
-    	Object data =new dao.PeliculaDAO().getPeliculas(id.equals("cartelera")? 1 : 2 , true);
-    	session.setAttribute("id", data == null? null :  "peliculas");
-    	session.setAttribute("data", data);
+    		Object data =new dao.PeliculaDAO().getPeliculas(id.equals("cartelera")? 1 : 2 , true);
+    		session.setAttribute("id", data == null? null :  "peliculas");
+    		session.setAttribute("data", data);
     }
     	
     	response.sendRedirect("index.jsp");
-    		
+    	
+    	
+    	
     }
-
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
 	}
-	
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		processRequest(request,response);
 	}
 
 }
